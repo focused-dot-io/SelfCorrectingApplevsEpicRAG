f7eb9cf4773ddab7d7ea48ba64a218cf4d0a6422
sarah.kainec <sarah.kainec@focusedlabs.io>
2024-05-19 15:42:17 -0600

    fixing issue with merging arrays in internet ask

--- ask_internet/ask_the_internet.py
+++ ask_internet/ask_the_internet.py
@@ -4,13 +4,16 @@ from langchain_core.documents import Document
 
 def ask_the_internet(state: dict) -> dict:
     question = state["question"]
+    documents = state["documents"]
 
     print(f"Asking the internet: {question}\n")
 
-    docs = search_internet(question)
-    web_results = [Document(page_content=d["content"], metadata={"source": d["url"]}) for d in docs]
+    web_docs = search_internet(question)
+    web_results = [Document(page_content=d["content"], metadata={"source": d["url"]}) for d in web_docs]
+
+    all_docs = documents + web_results
 
     return {
         **state,
-        "documents": web_results,
+        "documents": all_docs,
     }
